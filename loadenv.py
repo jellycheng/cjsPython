314677a0017b5c46afb4ad6561fcbac52700ff60
jellycheng <42282367@qq.com>
2016-07-29 12:59:12 +0800

    Create loadenv.py

---
+++ loadenv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+def load_dotenv(dotenv_path):
+    if not os.path.exists(dotenv_path):
+        return None
+    for k, v in parse_dotenv(dotenv_path):
+        os.environ.setdefault(k, v)
+    return True
+
+def parse_dotenv(dotenv_path):
+    with open(dotenv_path) as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('#') or '=' not in line:
+                continue
+            k, v = line.split('=', 1)
+            v = v.strip("'").strip('"')
+            yield k, v
+
+
+
+
+
+
